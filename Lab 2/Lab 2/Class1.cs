b1ddce2549779edcefbc117d9c5ce3d489d8b4af
Pablo Moyano <pamoyano@miuandes.cl>
2020-03-28 16:55:52 -0300

    first commit

---
+++ Lab 2/Lab 2/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    public class Canción
+    {
+        private string Name;
+        private string Album;
+        private string Artist;
+        private string Genre;
+        public Canción(string Namesong, string Albumsong, string Artistsong, string Genresong)
+        {
+            Name = Namesong;
+            Album = Albumsong;
+            Artist = Artistsong;
+            Genre = Genresong;
+        }
+        public string Informacion(string Namesong, string Albumsong,string Artistsong,string Genresong)
+        {
+            string info = "genero:" + Genresong + ", artista:" + Artistsong + ", album:" + Albumsong + ", nombre:" + Namesong;
+            return info;
+          }
+         
+
+
+    }
+}
