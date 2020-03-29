34f42339edd7cad79e71b607c926129d49977cf9
Pablo Moyano <pamoyano@miuandes.cl>
2020-03-29 19:06:06 -0300

    first increment advance

--- Lab 2/Lab 2/Class2.cs
+++ Lab 2/Lab 2/Class2.cs
@@ -10,6 +10,16 @@ namespace Lab_2
     {public Espotifai()
         { }
         public bool AgregarCancion(Canción cancion)
+        {foreach(string element in listamusical);
+                }
+        public void VerCanciones()
+        { }
+        public List<Canción> CancionesPorCriterio(String criterio, String valor)
+        { }
+        public bool GenerarPlaylist(String criterio, String valorCriterio, String
+nombrePlaylist)
+        { }
+        public String VerMisPlaylists()
         { }
     }
 }
--- Lab 2/Lab 2/Program.cs
+++ Lab 2/Lab 2/Program.cs
@@ -10,12 +10,22 @@ namespace Lab_2
     {
         static void Main(string[] args)
         {
+            List<string> listamusical = new List<string>();
             int choice;
             Console.WriteLine("Bienvenido a Espotifai,por favor elija la acción que deseé realizar:");
             Console.WriteLine("1:Ver las canciones,");
             Console.WriteLine("2:Agregar canción"); 
             Console.WriteLine("3:Salir del menu");
             choice = Convert.ToInt32(Console.ReadLine());
+            while (choice != 3) 
+            {if (choice==1)
+                {foreach (string element in listamusical)
+                        }
+             else if (choice==2)
+                { }
+             
+            }
+            Console.WriteLine("Gracias por usar el programa");
 
         }
     }
